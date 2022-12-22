7b25fbe330aff83a4c42ab6fa18692714a19acd1
Omar-Lamprea <lampreaomar@gmail.com>
2022-12-22 13:29:18 -0500

    changes in header, cards and icons

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,15 @@
 const toggleResquest = (e) =>{
+  // const p = nodo.children[1].children[0]
   const nodo = document.getElementById(e)
-  const p = nodo.children[1].children[0]
   const icon = nodo.children[0].children[1].children[0]
 
   if(nodo.classList.contains('open')){
     nodo.classList.remove('open')
-    // p.setAttribute('style', 'color:transparent')
-    // nodo.setAttribute('style', 'height: 65px')
-    icon.setAttribute('class', 'bi bi-plus-lg')
+    icon.classList.remove('rotateIcon1')
+    icon.classList.add('rotateIcon2')
   }else{
     nodo.classList.add('open')
-    // p.removeAttribute('style')
-    // nodo.removeAttribute('style')
-    icon.setAttribute('class', 'bi bi-dash-lg')
+    icon.classList.remove('rotateIcon2')
+    icon.classList.add('rotateIcon1')
   }
 }
