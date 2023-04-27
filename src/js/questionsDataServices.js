d62ca34ca0481088012e859432e3a01917b9c2dc
Omar-Lamprea <lampreaomar@gmail.com>
2023-04-27 15:47:13 -0500

    data services updated

--- src/js/questionsDataServices.js
+++ src/js/questionsDataServices.js
@@ -9,36 +9,36 @@ const cardListServices = [
     id: "card-2",
     class: "card-question",
     title: "Ventaja 2",
-    response: "Lorem, ipsum dolor sit elit. Doloremque, architecto iusto neque. Soluta unde."
+    response: "Integración de procesos de negocio de extremo a extremo en todas las funciones clave; Estrecha coordinación y control de los socios."
   },
   {
     id: "card-3",
     class: "card-question",
     title: "Ventaja 3",
-    response: "Lorem, ipsum dolor sit elit. Doloremque, architecto iusto neque. Soluta unde."
+    response: "Capacidades digitales avanzadas y servicios logísticos de próxima generación, incluido el soporte de comercio electrónico."
   },
   {
     id: "card-4",
     class: "card-question",
     title: "Ventaja 4",
-    response: "Lorem, ipsum dolor sit elit. Doloremque, architecto iusto neque. Soluta unde."  
+    response: "Optimización de ingresos a través de capacidades avanzadas de gestión de ingresos y rendimiento, y gestión de capacidad en tiempo real."  
   },
   {
     id: "card-5",
     class: "card-question",
     title: "Ventaja 5",
-    response: "Lorem, ipsum dolor sit elit. Doloremque, architecto iusto neque. Soluta unde."  
+    response: "Acceso bajo demanda a servicios y capacidad para escalar fácilmente"  
   },
   {
     id: "card-6",
     class: "card-question",
     title: "Ventaja 6",
-    response: "Lorem, ipsum dolor sit elit. Doloremque, architecto iusto neque. Soluta unde."  
+    response: "Cobertura de red ampliada para aerolíneas a través de interlínea sin fisuras, particularmente aerolíneas que utilizan la mejor plataforma de su clase."  
   },
   {
     id: "card-7",
     class: "card-question",
     title: "Ventaja 7",
-    response: "Lorem, ipsum dolor sit elit. Doloremque, architecto iusto neque. Soluta unde."  
+    response: "Las economías de escala y el apalancamiento de compras impulsado por el volumen con proveedores clave (por ejemplo, manipuladores) a través del servicio de múltiples aerolíneas y la agregación de sus volúmenes de carga. "  
   },
 ]
