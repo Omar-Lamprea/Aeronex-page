055c4259f70a37c973082de32ec0d10e1e0cd69e
Omar-Lamprea <lampreaomar@gmail.com>
2023-06-14 13:23:47 -0500

    alliance logo updated

--- src/js/allies.js
+++ src/js/allies.js
@@ -1,6 +1,6 @@
 const LOGO_DJ_CARGO = "./img/allies-Logo DJ.png"
 const LOGO_AIR_GENERAL = "./img/allies-AG Logo Cargo.png"
-const LOGO_ALLIANCE = "./img/allie-alliance-logo.svg"
+const LOGO_ALLIANCE = "./img/allies-AGI Logo Large Dark Blue.png"
 
 const alliesList = [
   {
@@ -50,37 +50,37 @@ const alliesList = [
   },
   {
     img: LOGO_ALLIANCE,
-    link: "https://www.allianceairlines.com.au/",
+    link: "https://www.allianceground.com/",
     stations: "MIA"
   },
   {
     img: LOGO_ALLIANCE,
-    link: "https://www.allianceairlines.com.au/",
+    link: "https://www.allianceground.com/",
     stations: "JFK"
   },
   {
     img: LOGO_ALLIANCE,
-    link: "https://www.allianceairlines.com.au/",
+    link: "https://www.allianceground.com/",
     stations: "EWR"
   },
   {
     img: LOGO_ALLIANCE,
-    link: "https://www.allianceairlines.com.au/",
+    link: "https://www.allianceground.com/",
     stations: "ATL"
   },
   {
     img: LOGO_ALLIANCE,
-    link: "https://www.allianceairlines.com.au/",
+    link: "https://www.allianceground.com/",
     stations: "SFO"
   },
   {
     img: LOGO_ALLIANCE,
-    link: "https://www.allianceairlines.com.au/",
+    link: "https://www.allianceground.com/",
     stations: "LAX"
   },
   {
     img: LOGO_ALLIANCE,
-    link: "https://www.allianceairlines.com.au/",
+    link: "https://www.allianceground.com/",
     stations: "LAS"
   },
   {
@@ -106,62 +106,6 @@ const alliesList = [
   },
   
 ]
-
-// const alliesList = [
-//   {
-//     img: LOGO_DJ_CARGO,
-//     link: "https://djcargo.com/",
-//     stations: "STI - SDQ"
-//   },
-//   {
-//     img: "./img/allies-LOG - WFS CMJN.png",
-//     link: "https://www.wfs.aero/",
-//     stations: ""
-//   },
-//   {
-//     img: LOGO_AIR_GENERAL,
-//     link: "https://airgeneral.com/",
-//     stations: "FLL - TPA - MCO - ORD"
-//   },
-//   {
-//     img: "./img/allies-JetBlueLogo.png",
-//     link: "https://www.jetblue.com/",
-//     stations: ""
-//   },
-//   {
-//     img: "./img/allies-LOGO_GSA_FORCE.png",
-//     link: "https://gsaforce.com/",
-//     stations: ""
-//   },
-//   {
-//     img: LOGO_ALLIANCE,
-//     link: "https://www.allianceairlines.com.au/",
-//     stations: "MIA - JFK - EWR - ATL - SFO - LAX - LAS"
-//   },
-//   {
-//     img: "./img/allies-GMD-LOGO.png",
-//     link: "https://www.gmdpr.com/",
-//     stations: ""
-//   },
-  
-//   {
-//     img: "./img/allies-ESCM LOGO.png",
-//     link: "https://www.eckertseamans.com/",
-//     stations: ""
-//   },
-//   {
-//     img: "./img/allies-IBS-Logo.png",
-//     link: "https://www.ibsplc.com/",
-//     stations: ""
-//   },
-//   {
-//     img: "./img/allies-Logo-Altitute.png",
-//     link: "https://www.tenaciousaltitude.com/",
-//     stations: ""
-//   },
-  
-// ]
-
 
 const alliesContainer = document.getElementById('container-allies')
 if(alliesContainer){
